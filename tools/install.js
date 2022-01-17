ba47a37b4d8d7e16f80fc2a42423623605e991c3
Dan Boger <dan@peeron.com>
2022-01-17 15:55:57 -0800

    Support installing controllers, workers, and using ALL for the target

--- tools/install.js
+++ tools/install.js
@@ -1,8 +1,26 @@
+import * as hosts from "/lib/hosts.js";
+
 /** @param {NS} ns **/
 export async function main(ns) {
     var target = ns.args[0];
     var tool = ns.args[1];
     var args = ns.args.splice(2);
+
+    if (target == "all") {
+        var hs = hosts.hosts(ns).filter((h) => {return !h.host.startsWith("pserv-")});
+        var req = ns.getScriptRam("/daemons/weakener.js");
+        for (var i in hs) {
+            var h = hs[i];
+            if (h.host == "hone") {
+                continue;
+            }
+            ns.tprintf("installing weakener on %s", h.host);
+            if (ns.getServerMaxRam(h.host) > req) {
+                await installWeaken(ns, h.host);
+            }
+        }
+        return;
+    }
 
     switch (tool) {
         case "batch":
@@ -10,6 +28,12 @@ export async function main(ns) {
             break;
         case "weaken":
             await installWeaken(ns, target);
+            break;
+        case "controller":
+            await installController(ns, target);
+            break;
+        case "worker":
+            await installWorker(ns, target);
             break;
         default:
             ns.tprintf("Looking up what should be installed on %s", target);
@@ -19,7 +43,8 @@ export async function main(ns) {
                 ns.tprintf("Installing batch on %s to hack %s", h.worker, h.target);
                 await installBatch(ns, h.worker, h.target);
             } else {
-                ns.tprintf("No entry found for %s", target);
+                ns.tprintf("Installing weakener on %s", target);
+                await installWeaken(ns, target);
             }
     }
 
@@ -28,7 +53,7 @@ export async function main(ns) {
 export async function installBatch(ns, worker, target) {
     if (target != "home") {
         await ns.scp(
-            ["weaken.js", "hack.js", "grow.js", "/daemons/batch.js", 
+            ["weaken.js", "hack.js", "grow.js", "/daemons/batch.js", "/lib/assignments.txt",
                 "/lib/fmt.js", "/lib/log.js"], "home", worker);
         ns.killall(worker);
     }
@@ -47,6 +72,42 @@ export async function installWeaken(ns, worker) {
     if (ns.getServerMaxRam(worker) > ns.getScriptRam("/daemons/weakener.js")) {
         if (!ns.exec("/daemons/weakener.js", worker)) {
             ns.tprintf("Failed to launch weakener on %s", worker);
+        }
+    }
+}
+
+/**
+ * @param {NS} ns
+ * @param {string} worker
+ */
+export async function installController(ns, worker) {
+    if (worker != "home") {
+        await ns.scp(["/daemons/controller.js", "/lib/log.js", "/lib/fmt.js"], "home", worker);
+        ns.killall(worker);
+    }
+    var ram = ns.getServerMaxRam(worker) - ns.getServerUsedRam(worker);
+    var req = ns.getScriptRam("/daemons/controller.js");
+    if (ram > req) {
+        if (!ns.exec("/daemons/controller.js", worker)) {
+            ns.tprintf("Failed to launch controller on %s", worker);
+        }
+    }
+}
+
+/**
+ * @param {NS} ns
+ * @param {string} worker
+ */
+export async function installWorker(ns, worker) {
+    if (worker != "home") {
+        await ns.scp(["/daemons/worker.js", "/lib/log.js", "/lib/fmt.js"], "home", worker);
+        ns.killall(worker);
+    }
+    var ram = ns.getServerMaxRam(worker) - ns.getServerUsedRam(worker);
+    var req = ns.getScriptRam("/daemons/worker.js");
+    if (ram > req) {
+        if (!ns.exec("/daemons/worker.js", worker, ram/req)) {
+            ns.tprintf("Failed to launch worker on %s", worker);
         }
     }
 }
