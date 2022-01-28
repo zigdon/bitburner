c5098842c3f59b7a488bcf340dec063a2436c368
Dan Boger <dan@peeron.com>
2022-01-28 09:14:22 -0800

    Add UI elements to see how effective our sharing is

--- daemons/share.js
+++ daemons/share.js
@@ -1,32 +1,36 @@
+import * as zui from "/lib/ui.js";
+
 /** @param {NS} ns **/
 export async function main(ns) {
     ns.disableLog("sleep");
     var reserve = parseMemory(ns.args[0]);
     ns.print("Reserving ", reserve, " GB");
     var hostname = ns.getHostname();
-    var thread = ns.getScriptRam("/tools/share.js");
+    var thread = ns.getScriptRam("/bin/share.js");
     if (hostname == "home") {
-        ns.tail();
+        zui.customOverview("sharePower", "Sharing");
     }
     ns.print("Each thread needs ", thread, " GB");
+    ns.atExit(function() {zui.rmCustomOverview("sharePower")});
     while(true) {
         await ns.sleep(Math.random()*10000+1000);
         var avail = ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname);
         avail -= reserve;
         var n = avail/thread;
-        var pid = ns.run("/tools/share.js", n);
+        if (n < 1) {
+            continue;
+        }
+        var pid = ns.run("/bin/share.js", n);
         var sp = 1;
         while (ns.isRunning(pid, hostname)) {
             var newSP = ns.getSharePower();
-            if (sp != newSP) {
-                ns.clearLog();
-                ns.print("Share power: ", ns.getSharePower());
+            if (sp != newSP && hostname == "home") {
+                zui.setCustomOverview("sharePower", ns.sprintf("+%.2f%%", (ns.getSharePower()-1)*100));
                 sp = newSP;
             }
             await ns.sleep(500);
         }
     }
-
 }
 
 function parseMemory(n) {
