8076d0f319612d0a0af76e08cd1f1d53e8f08364
Dan Boger <dan@peeron.com>
2022-01-31 22:04:39 -0800

    Show obsolete servers when lssrv

--- tools/lsservers.js
+++ tools/lsservers.js
@@ -3,23 +3,28 @@ import * as fmt from "/lib/fmt.js";
 export async function main(ns) {
     var srvs = ns.getPurchasedServers();
     srvs.sort();
-    for (var s of srvs.sort((a,b) => { return b.substring(b.indexOf("-")) - a.substring(a.indexOf("-")) })) {
+    for (var s of srvs.sort((a, b) => { return b.substring(b.indexOf("-")) - a.substring(a.indexOf("-")) })) {
         var maxRam = ns.getServerMaxRam(s);
         var usedRam = ns.getServerUsedRam(s);
+        var obs = ns.fileExists("/obsolete.txt", s) ? "[obsolete] " : "";
         var procs = ns.ps(s)
-        ns.tprintf("%8s: %6s/%6s GB, %s", s, fmt.int(usedRam), fmt.int(maxRam), await summarise(ns, procs));
+        ns.tprintf("%8s: %6s/%6s GB, %s%s",
+            s, fmt.int(usedRam),
+            fmt.int(maxRam), 
+            obs, 
+            await summarise(ns, procs));
     }
 }
 
 async function summarise(ns, procs) {
     var c = new Map();
     for (var p of procs) {
-        var f = p.filename.substring(p.filename.indexOf("/", 1)+1);
+        var f = p.filename.substring(p.filename.indexOf("/", 1) + 1);
         await ns.sleep(5);
         if (!c.has(f)) {
             c.set(f, 0);
         }
-        c.set(f, c.get(f)+p.threads);
+        c.set(f, c.get(f) + p.threads);
     }
 
     var res = []
