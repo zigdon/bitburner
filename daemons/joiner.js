95145a6f8ff6781ea802bcb40232ce32677976e9
Dan Boger <dan@peeron.com>
2022-02-05 23:33:48 -0800

    joiner: automatically accept/decline faction invites

---
+++ daemons/joiner.js
@@ -0,0 +1,26 @@
+import {toast} from "/lib/log.js";
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    var rejected = [];
+    while (true) {
+        var invs = ns.checkFactionInvitations();
+        for (var i of invs) {
+            if (rejected.indexOf(i) >= 0) {
+                continue;
+            }
+            var owned = ns.getOwnedAugmentations(true);
+            var missing = ns.getAugmentationsFromFaction(i).filter(a => owned.indexOf(a) == -1);
+            if (missing.length > 0) {
+                await toast(ns, "Accepting new invitation from %s, %d augs missing", i, missing.length);
+                if (!ns.joinFaction(i)) {
+                    await toast(ns, "Failed to join %s for some reasons!", i, {level: "error", timeout: 10000});
+                }
+            } else {
+                await toast(ns, "Rejecting new invitation from %s", i);
+                rejected.push(i)
+            }
+        }
+        await ns.sleep(30000);
+    }
+}
