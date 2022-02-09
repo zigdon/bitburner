e805b37d649d9fde0c4a7d2640b18efe5154ea8e
zigdon <dan@peeron.com>
2022-02-08 20:43:04 -0800

    Create mini versions to start up a life of crime

---
+++ mini/docrime.js
@@ -0,0 +1,12 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    ns.disableLog("sleep");
+    while (!ns.gang.createGang("Slum Snakes")) {
+        ns.tail();
+        ns.commitCrime("Homicide");
+        while (ns.isBusy()) {
+            await ns.sleep(100);
+        }
+        ns.print(ns.getPlayer().numPeopleKilled, " kills");
+    }
+}
