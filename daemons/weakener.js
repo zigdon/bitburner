04a4db666d56d9fcfd80fda0804cbfc00a8bca71
zigdon <dan@peeron.com>
2022-02-16 17:04:17 -0800

    Support obsoleting servers, use port names

--- daemons/weakener.js
+++ daemons/weakener.js
@@ -1,10 +1,13 @@
 import * as fmt from "/lib/fmt.js";
 import {netLog} from "/lib/log.js";
+import {getPorts} from "/lib/ports.js";
+import {readAssignments} from "/lib/assignments.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
     // Looking through the assigned targets, run weaken on the one with the
     // highest sec
+    const ports = getPorts();
 
     // How much ram to leave unused
     var reserve = ns.args[0];
@@ -18,6 +21,10 @@ export async function main(ns) {
     var hostname = ns.getHostname();
     var sRam = ns.getScriptRam(script);
     while (true) {
+        if (ns.fileExists("/obsolete.txt")) {
+            await netLog(ns, "Server obsolete, shutting down...");
+            return;
+        }
         await ns.sleep(2000+Math.random()*10000);
         if (hostname != "home") {
             await ns.scp("/conf/assignments.txt", "home", hostname);
@@ -27,16 +34,14 @@ export async function main(ns) {
             await netLog(ns, "No assignments found, exiting");
             return;
         }
-        var assignments = [];
-        data.split("\n").forEach((l) => {
-            var bits = l.trim().split("\t");
-            if (bits[1].startsWith("<")) {
-                return;
-            }
-            assignments.push(bits[1]);
-        })
+        var assignments = readAssignments(ns)  // Read all the targets
+            // figure out how much security we can get rid of
+            .map(a => a.targets.map(t => [t, ns.getServerSecurityLevel(t)-ns.getServerMinSecurityLevel(t)]))
+            .flat(1)  // one entry per target, not per worker
+            .sort((a,b) => b[1]-a[1]) // find who needs the most help
+            .map(a => a[0]);  // extract the target name
 
-        var peer = ns.readPort(6);
+        var peer = ns.readPort(ports.WEAKENERS);
         if (peer.startsWith("NULL")) {
             peer = "";
         }
@@ -55,7 +60,11 @@ export async function main(ns) {
         } else {
             target = assignments[Math.floor(Math.random()*assignments.length)];
         }
-        await ns.tryWritePort(6, target);
+        if (!target) {
+            await netLog(ns, "No target , quitting");
+            return;
+        }
+        await ns.tryWritePort(ports.WEAKENERS, target);
 
         var threads = Math.floor(
             ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname) - reserve)/sRam;
@@ -68,17 +77,15 @@ export async function main(ns) {
             continue;
         }
 
-        if (!target) {
-            await netLog(ns, "No target , quitting");
-            return;
-        }
-        await netLog(ns, "Weakening %s with %s threads", target, fmt.int(threads));
+        var start = ns.getServerSecurityLevel(target);
+        await netLog(ns, "Weakening %s with %s threads, security: %.2f", target, fmt.int(threads), start);
         var pid = ns.run(script, threads, target);
         if (pid) {
             await ns.sleep(ns.getWeakenTime(target));
             while (ns.scriptRunning(script, hostname)) {
                 await ns.sleep(1000);
             }
+            await netLog(ns, "Done weakening %s with %s threads, security: %.2f", target, fmt.int(threads), ns.getServerSecurityLevel(target));
         }
     }
 
