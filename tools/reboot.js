75012f2ae93b443b8ac1359f5132654e5a1a770d
zigdon <dan@peeron.com>
2022-02-16 17:03:30 -0800

    reboot supports --donate, --now, --batch, and --any. Also uses a persistent log file for events

--- tools/reboot.js
+++ tools/reboot.js
@@ -4,17 +4,125 @@ import * as fmt from "/lib/fmt.js";
 /** @param {NS} ns **/
 export async function main(ns) {
     ns.disableLog("sleep");
+    var logFile = "/log/keep/reboot.txt";
+    async function log(t, ...args) {
+        var ts = new Date().toLocaleString();
+        t = "\n" + ts + " - " + t;
+        await ns.write(logFile, ns.sprintf(t, ...args), "a");
+    }
     var flags = ns.flags([
         ["time", null],
         ["money", null],
         ["hack", null],
         ["rep", null],
-        ["now", null],
+        ["donate", null],
+        ["now", false],
+        ["batch", false],
+        ["any", false],
     ])
+
+    var [waitDesc, waitFuncs] = parseFlags(ns, flags);
+
+    var msg = "";
+    if (waitDesc.length > 0) {
+        var any = waitDesc.length == 1 ? "" : flags.any ? "(any) " : "(all) "
+        msg = "Waiting for " + any + waitDesc.join(", ") + " then install augs and reboot?";
+    } else {
+        msg = "Install augs and reboot?";
+    }
+    if (!flags.batch && !await ns.prompt(msg)) {
+        return;
+    }
+
+    if (waitFuncs.length > 0) {
+        var startWait = Date.now();
+        ns.toast(`Waiting for ${flags.any ? "(any) " : ""}${waitDesc.join(", ")}...`, "warning", null);
+        var check = flags.any ? (t, f) => t || f() : (t, f) => t && f();
+        while (!waitFuncs.reduce(check, !flags.any)) {
+            ns.print(`Still waiting for ${(flags.any ? "any of: " : "all of: ")}${waitDesc.join(", ")}`);
+            await ns.sleep(5000);
+        }
+        ns.toast(`Wait complete, after ${fmt.time(Date.now()-startWait)}`, "success", 30000);
+        await log("Wait complete after %s", fmt.time(Date.now()-startWait));
+    }
+
+    if (ns.isRunning("/daemons/buyer.js", "home")) {
+        ns.kill("/daemons/buyer.js", "home");
+    }
+
+    var pid = ns.run("/tools/lsaugs.js", 1, "buy", "hack", "--quiet");
+    while (ns.isRunning(pid, "home")) {
+        await ns.sleep(100);
+    }
+    var installedAugs = ns.getOwnedAugmentations(false);
+    var allAugs = ns.getOwnedAugmentations(true);
+    var newAugs = [];
+    for (var a of allAugs) {
+        if (installedAugs.indexOf(a) == -1) {
+            newAugs.push(a);
+        }
+    }
+    await log("New augs:\n%s", newAugs.join("\n"));
+
+    // Buy any server upgrades
+    var c = 0;
+    while (ns.getServerMoneyAvailable("home") > ns.getUpgradeHomeCoresCost()) {
+        if (!ns.upgradeHomeCores()) {
+            break;
+        }
+        c++;
+    }
+    ns.toast(`Upgraded home cores ${c} times`, "success", null);
+    await log(`Upgraded home cores ${c} times`);
+
+    c = 0;
+    while (ns.getServerMoneyAvailable("home") > ns.getUpgradeHomeRamCost()) {
+        if (!ns.upgradeHomeRam()) {
+            break;
+        }
+        c++;
+    }
+    ns.toast(`Upgraded RAM ${c} times`, "success", null);
+    await log(`Upgraded RAM ${c} times to ${fmt.memory(ns.getServerMaxRam("home"))}`);
+
+    if (!flags.now) {
+        ns.toast("Reboot in 1 minute!", "warning", 30000);
+        await ns.sleep(30000);
+        ns.toast("Reboot in 30 seconds!", "warning", 20000);
+        await ns.sleep(20000);
+        for (var i=10; i>0; i--) {
+            ns.toast(`Reboot in ${i} seconds!`, "warning", 1000);
+            await ns.sleep(1000);
+        }
+    }
+
+    // Buy any possible NeuroFlux Governor
+    // Find the faction with the most rep
+    c = 0;
+    for(var fact of getFactions().map(f => [f, ns.getFactionRep(f)]).sort((a,b) => b[1]-a[1])) {
+        while (ns.purchaseAugmentation(fact[0], "NeuroFlux Governor")) {
+            c++;
+        }
+        if (c > 0) {
+            ns.toast(`Bought NeuroFlux Governors from ${fact[0]} ${c} times`, "success", null);
+            await log(`Bought NeuroFlux Governors from ${fact[0]} ${c} times`);
+            break;
+        }
+    }
+
+    // Install any augs
+    ns.installAugmentations("/bin/reboot-init.js");
+}
+
+/**
+ * @param {NS} ns
+ * @param {Object} flags
+ */
+function parseFlags(ns, flags) {
     var waitDesc = [];
     var waitFuncs = [];
     if (flags.time) {
-        waitTime = fmt.parseTime(flags.time);
+        var waitTime = fmt.parseTime(flags.time);
         waitDesc.push(fmt.time(waitTime));
         var start = Date.now();
         waitFuncs.push(function() {
@@ -55,79 +163,11 @@ export async function main(ns) {
         }
         waitDesc.push(`can donate to ${fact}`);
         waitFuncs.push(function() {
-            return ns.getFactionFavorGain(fact) + ns.getFactionFavor(fact) > ns.getFavorToDonate();
+            var pending = ns.getPlayer().currentWorkFactionName == fact ? ns.getPlayer().workRepGained : 0;
+            var extra = Math.log(pending / 25000 + 1) / Math.log(1.02)
+            return ns.getFactionFavorGain(fact) + ns.getFactionFavor(fact) + extra > ns.getFavorToDonate();
         })
     }
 
-    var msg = "";
-    if (waitDesc.length > 0) {
-        msg = "Waiting for " + waitDesc.join(", ") + " then install augs and reboot?";
-    } else {
-        msg = "Install augs and reboot?";
-    }
-    if (!await ns.prompt(msg)) {
-        return;
-    }
-
-    if (waitFuncs.length > 0) {
-        var startWait = Date.now();
-        ns.toast(`Waiting for ${waitDesc.join(", ")}...`, "warning", null);
-        while (!waitFuncs.reduce((t, f) => t && f(), true)) {
-            ns.print(`Still waiting for ${waitDesc.join(", ")}`);
-            await ns.sleep(5000);
-        }
-        ns.toast(`Wait complete, after ${fmt.time(Date.now()-startWait)}`, "success", 30000);
-    }
-
-    var pid = ns.run("/tools/lsaugs.js", 1, "buy", "hack", "--quiet");
-    while (ns.isRunning(pid, "home")) {
-        await ns.sleep(100);
-    }
-
-    // Buy any server upgrades
-    var c = 0;
-    while (ns.getServerMoneyAvailable("home") > ns.getUpgradeHomeCoresCost()) {
-        if (!ns.upgradeHomeCores()) {
-            break;
-        }
-        c++;
-    }
-    ns.toast(`Upgraded home cores ${c} times`, "success", null);
-
-    c = 0;
-    while (ns.getServerMoneyAvailable("home") > ns.getUpgradeHomeRamCost()) {
-        if (!ns.upgradeHomeRam()) {
-            break;
-        }
-        c++;
-    }
-    ns.toast(`Upgraded RAM ${c} times`, "success", null);
-
-    // Buy any possible NeuroFlux Governor
-    // Find the faction with the most rep
-    c = 0;
-    for(var fact of getFactions().map(f => [f, ns.getFactionRep(f)]).sort((a,b) => b[1]-a[1])) {
-        ns.toast(`Trying to buy extra augs from ${fact[0]}`);
-        while (ns.purchaseAugmentation(fact[0], "NeuroFlux Governor")) {
-            c++;
-        }
-        if (c > 0) {
-            ns.toast(`Bought NeuroFlux Governors from ${fact[0]} ${c} times`, "success", null);
-            break;
-        }
-    }
-
-    if (!flags.now) {
-        ns.toast("Reboot in 1 minute!", "warning", 30000);
-        await ns.sleep(30000);
-        ns.toast("Reboot in 30 seconds!", "warning", 20000);
-        await ns.sleep(20000);
-        for (var i=10; i>0; i--) {
-            ns.toast(`Reboot in ${i} seconds!`, "warning", 1000);
-            await ns.sleep(1000);
-        }
-    }
-
-    // Install any augs
-    ns.installAugmentations("/bin/reboot-init.js");
+    return [waitDesc, waitFuncs];
 }
--- tools/rmlogs.js
+++ tools/rmlogs.js
@@ -2,6 +2,7 @@
 export async function main(ns) {
     var logs = ns.ls("home", "/log/");
     for (var l in logs) {
+        if (logs[l].includes("keep/")) { continue }
         if(logs[l].split("/").length == 4) {
             ns.rm(logs[l], "home");
         };
