79728d7e9a764e5752500a7c9ca12fc7e1c55a2f
zigdon <dan@peeron.com>
2022-02-24 08:29:01 -0800

    Train abilities before starting to do crimes

--- tools/docrime.js
+++ tools/docrime.js
@@ -1,4 +1,6 @@
 import * as fmt from "/lib/fmt.js";
+import {keep} from "/lib/log.js";
+import {getPorts} from "/lib/ports.js";
 
 const crimes = [
     "shoplift",
@@ -24,7 +26,13 @@ function getCrimeValues(ns, priority) {
     for (let crime of crimes) {
         const crimeStats = ns.getCrimeStats(crime);
         // using endswith lets us use 'exp' or 'xp' to combine all stat xp
-        const relevantKeys = Object.keys(crimeStats).filter((k) => k.endsWith(priority));
+        var relevantKeys;
+        if (priority.includes("_")) {
+            const stats = priority.split("_")[0];
+            relevantKeys = Object.keys(crimeStats).filter((k) => k.startsWith(stats));
+        } else {
+            relevantKeys = Object.keys(crimeStats).filter((k) => k.endsWith(priority));
+        }
         const relevantValues = relevantKeys.map((k) => crimeStats[k]);
         crimeValues[crime] = relevantValues.reduce((a, b) => a + b) / crimeStats.time;
     }
@@ -32,8 +40,25 @@ function getCrimeValues(ns, priority) {
 }
 
 /** @param {NS} ns **/
+async function checkStats(ns) {
+    const gym = "powerhouse gym";
+    for (var name of [ "strength",  "defense",  "dexterity",  "agility"]) {
+        var p = ns.getPlayer();
+        while (p[name] < 50) {
+            ns.gymWorkout(gym, name, true);
+            await ns.sleep(100);
+            p = ns.getPlayer();
+        }
+        ns.stopAction()
+    }
+}
+
+/** @param {NS} ns **/
 export async function main(ns) {
     ns.disableLog("asleep");
+
+    await checkStats(ns);
+
     // possible priorities: karma, kills, money, STAT_exp, xp
     const priority = ns.args[0] || "money";
     const crimeValues = getCrimeValues(ns, priority);
@@ -47,22 +72,41 @@ export async function main(ns) {
         // other rewards only happen if you succeed
         (crime) => crimeValues[crime] * ns.getCrimeChance(crime);
     const startFact = ns.getPlayer().currentWorkFactionName;
+    var startWork = "";
+    switch (ns.getPlayer().currentWorkFactionDescription) {
+        case "carrying out field missions":
+            startWork = "Field Work";
+            break;
+    }
     ns.atExit(() => {
-        if (startFact) {
-            ns.stopAction();
-            ns.workForFaction(startFact, "Security Work", false);
-        }
+        try {
+            if (startFact) {
+                ns.stopAction();
+                ns.workForFaction(startFact, "Field Work", false);
+            }
+        } catch {}
     })
     while (true) {
         // do this inside the loop because success chance changes
         crimes.sort((a, b) => ev(b) - ev(a));
         // if we don't keep tail up it's hard to stop this script
         ns.tail();
-        ns.clearLog();
         ns.print(`Expected rate: ${fmt.large(ev(crimes[0])*1000)} ${priority}/s`);
-        await ns.commitCrime(crimes[0]);
+        if (priority == "karma") {
+            ns.print(`Kills: ${fmt.large(ns.getPlayer().numPeopleKilled)}`);
+        }
+        ns.commitCrime(crimes[0]);
         while (ns.isBusy()) {
             await ns.asleep(10);
+        }
+        if (priority == "karma" && ns.gang.createGang("Slum Snakes")) {
+            var player = ns.getPlayer();
+            await keep(ns, "Gang created %s into BN%d", fmt.time(player.playtimeSinceLastBitnode), player.bitNodeN);
+            ns.tprintf("Gang created %s into the node", player.playtimeSinceLastBitnode);
+            let port = getPorts().GANGMGR;
+            await ns.writePort(port, "focus respect");
+            await ns.writePort(port, "limit 50m");
+            ns.spawn("/daemons/gangmgr.js");
         }
     }
 }
