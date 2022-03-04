894acd558af52252447d1951897bc674c92fdac2
zigdon <dan@peeron.com>
2022-03-03 23:16:21 -0800

    corpmon can manage warehouse size, increase offices, do research

--- daemons/corpmon.js
+++ daemons/corpmon.js
@@ -1,65 +1,199 @@
 import * as fmt from "/lib/fmt.js";
-import {toast} from "/lib/log.js";
-import {getPorts} from "/lib/ports.js";
+import { netLog, toast } from "/lib/log.js";
+import { ports } from "/lib/ports.js";
+import { expandOffice } from "/lib/corp.js";
 
-const ports = getPorts();
+const researchCity = "Aevum";
+let util = {};
 
 /** @param {NS} ns **/
 export async function main(ns) {
   ns.disableLog("ALL");
-  let corp = ns.corporation.getCorporation();
-  if (!corp) {
-      toast(ns, "No corporation found, quitting.", {level: "warning"});
-      return;
-  }
+  ns.tail();
 
-  let lastPrice = 0;
+  let lastUpgrades = 0;
+  let lastTick = 0;
+  let utilReset = 0;
   await ns.writePort(ports.UI, "create corp Corp");
   while (true) {
-      // check product development
-      let progress = await devProducts(ns);
+    let now = Date.now();
+    // check product development
+    let progress = await devProducts(ns);
 
-      if (Date.now() - lastPrice > 20000) {
-        // check product pricing
-        await checkPrices(ns);
-        lastPrice = Date.now();
+    // Keep track of warehouse utilisation
+    if (now - utilReset > 300000) {
+      util = {};
+    }
+    for (let d of ns.corporation.getCorporation().divisions) {
+      util[d.name] ||= {};
+      for (let c of d.cities) {
+        util[d.name][c] ||= 0;
+        let w = ns.corporation.getWarehouse(d.name, c);
+        if (w.sizeUsed / w.size > util[d.name][c]) {
+          util[d.name][c] = w.sizeUsed / w.size;
+        }
       }
-      await printSummary(ns, progress);
-      // check commands
-      // check warehouse util, upgrade?
-      // buy gear?
-      // increase office size?
-      await ns.sleep(1000);
+    }
+
+    if (ns.corporation.getCorporation().funds != lastTick) {
+      lastTick = ns.corporation.getCorporation().funds;
+      // check product pricing
+      await checkPrices(ns);
+    }
+    await printSummary(ns, progress);
+    // check commands
+
+    if (now - lastUpgrades > 60000) {
+      await checkUpgrades(ns);
+      lastUpgrades = now;
+    }
+    // buy gear?
+    await ns.sleep(200);
+  }
+}
+
+/**
+ * @param {NS} ns
+ */
+async function checkUpgrades(ns) {
+  // corp upgrades we want at at least lvl 20:
+  let upgrades = [
+    "FocusWires",
+    "Neural Accelerators",
+    "Speech Processor Implants",
+    "Nuoptimal Nootropic Injector Implants",
+    "Smart Factories",
+    "Smart Storage",
+  ];
+
+  // Upgrades once we get up to speed
+  let extraUpgrades = [
+    "DreamSense",
+    "Project Insight",
+    "ABC SalesBots",
+  ];
+
+  let cur = upgrades.map(u => ({
+    name: u,
+    lvl: ns.corporation.getUpgradeLevel(u),
+    cost: ns.corporation.getUpgradeLevelCost(u),
+  }));
+  if (cur.every(u => u.lvl >= 20)) {
+    cur.push(...extraUpgrades.map(u => ({
+      name: u,
+      lvl: ns.corporation.getUpgradeLevel(u),
+      cost: ns.corporation.getUpgradeLevelCost(u),
+    })))
+  }
+
+  cur.sort((a, b) => a.cost - b.cost);
+  let next = cur[0];
+  if (cur.length > 0 && next.cost < ns.corporation.getCorporation().funds / cur.lvl < 20 ? 5 : 100) {
+    await netLog(ns, "Upgrading %s to level %d for %s",
+      next.name, next.lvl + 1, fmt.money(next.cost));
+    ns.corporation.levelUpgrade(next.name);
+  }
+
+  if (ns.corporation.getUpgradeLevelCost("Wilson Analytics") < ns.corporation.getCorporation().funds / 5) {
+    await netLog(ns, "Upgrading wilson level")
+    ns.corporation.levelUpgrade("Wilson Analytics");
+  }
+
+  for (let div of ns.corporation.getCorporation().divisions) {
+    for (let c of div.cities) {
+      if (util[div.name][c] > 0.8 && ns.corporation.getUpgradeWarehouseCost(div.name, c) < (div.lastCycleRevenue - div.lastCycleExpenses) * 60) {
+        await toast(ns, "Upgrading warehouse for %s in %s", div.name, c);
+        ns.corporation.upgradeWarehouse(div.name, c);
+        util[div.name][c] = 0;
+      }
+    }
+
+    if (div.type == "Agriculture") {
+      continue;
+    }
+
+    // If we can, research these:
+    let goals = [
+      "Hi-Tech R&D Laboratory",
+      "Market-TA.I",
+      "Market-TA.II",
+      "Drones",
+      "Overclock",
+      "Drones - Assembly",
+      "uPgrade: Fulcrum",
+      "uPgrade: Capacity.I",
+      "uPgrade: Capacity.II",
+    ];
+
+    for (let r of goals) {
+      if (ns.corporation.hasResearched(div.name, r)) {
+        continue;
+      }
+      if (ns.corporation.getResearchCost(div.name, r) < div.research) {
+        await netLog(ns, "Researching %s at %s", r, div.name);
+        ns.corporation.research(div.name, r);
+      } else {
+        break;
+      }
+    }
+
+    let sizes = div.cities.map(c => ({ name: c, size: ns.corporation.getOffice(div.name, c).size })).filter(c => c.name != researchCity).sort((a, b) => a.size - b.size);
+    let smallest = sizes[0];
+    let upgrade = smallest.size / 3;
+    if (upgrade < 9) { upgrade = 9; }
+    if (smallest.size < ns.corporation.getOffice(div.name, researchCity).size / 9 &&
+      ns.corporation.getOfficeSizeUpgradeCost(div.name, smallest.name, upgrade) < ns.corporation.getCorporation().funds / 5) {
+      if (smallest.size < 100) {
+        await toast(ns, "Upgrading sales office %s from %s for %s", smallest.name, fmt.large(smallest.size, { digits: 0 }), div.name);
+      } else {
+        await netLog(ns, "Upgrading sales office %s from %s for %s", smallest.name, fmt.large(smallest.size, { digits: 0 }), div.name);
+      }
+      ns.corporation.upgradeOfficeSize(div.name, smallest.name, upgrade);
+      await hireSales(ns, div.name, smallest.name);
+    }
+
+    if (ns.corporation.getOfficeSizeUpgradeCost(div.name, researchCity, 15) < ns.corporation.getHireAdVertCost(div.name)) {
+      await toast(ns, "Upgrading research office for %s", div.name);
+      ns.corporation.upgradeOfficeSize(div.name, researchCity, 15);
+      await hireResearchers(ns, div.name);
+    }
+
+    if (ns.corporation.getHireAdVertCost(div.name) < ns.corporation.getCorporation().funds / 5) {
+      await netLog(ns, "Hiring AdVert for %s", div.name);
+      ns.corporation.hireAdVert(div.name);
+    }
   }
 }
 
 /*
-* @param {NS} ns 
+* @param {NS} ns
 * @param {object} progress 
 */
 async function printSummary(ns, progress) {
-    ns.clearLog();
-    let corp = ns.corporation.getCorporation();
-    ns.print(corp.name.toUpperCase() + 
-      `   Profits: ${fmt.money(corp.revenue-corp.expenses)}  Funds: ${fmt.money(corp.funds)}`);
-    let data = [];
-    for (let div of corp.divisions) {
-      data.push([
-        div.name,
-        div.type,
-        div.lastCycleRevenue-div.lastCycleExpenses,
-        div.prodMult,
-        div.popularity,
-        div.awareness,
-        progress[div.name] ? fmt.pct(progress[div.name]) : "N/A",
-      ])
-    }
-    ns.print(fmt.table(data, [
-      "division name", "type", ["profits", fmt.money],
-      ["production", fmt.large], ["popularity", fmt.large], ["awareness", fmt.large],
-      "Product Dev",
-    ]))
-    await ns.writePort(ports.UI, `update corp ${fmt.money(corp.revenue-corp.expenses)}/2`);
+  ns.clearLog();
+  let corp = ns.corporation.getCorporation();
+  ns.print(corp.name.toUpperCase() +
+    `   Profits: ${fmt.money(corp.revenue - corp.expenses)}/s  Funds: ${fmt.money(corp.funds)}`);
+  let offer = ns.corporation.getInvestmentOffer();
+  if (offer.funds > 0) {
+    ns.print(`Available invenstment offer: ${fmt.money(offer.funds)} for ${fmt.large(offer.shares)} shares`);
+  }
+
+  let data = [];
+  for (let div of corp.divisions) {
+    data.push([
+      div.name,
+      div.type,
+      div.lastCycleRevenue - div.lastCycleExpenses,
+      div.prodMult,
+      progress[div.name] ? fmt.pct(progress[div.name]) : "N/A",
+    ])
+  }
+  ns.print(fmt.table(data, [
+    "division name", "type", ["profits", fmt.money],
+    ["production", fmt.large], "Product Dev",
+  ]))
+  await ns.writePort(ports.UI, `update corp ${fmt.money(corp.revenue - corp.expenses)}/s`);
 }
 
 /** @param {NS} ns **/
@@ -75,39 +209,41 @@ async function checkPrices(ns) {
       if (p.developmentProgress < 100) {
         continue;
       }
-      // If these work, the rest doesn't matter
-      let auto = false;
-      if (ns.corporation.hasResearched(div.name, "MarketTA1")) {
-        ns.corporation.setProductMarketTA1(div.name, name, true);
-        auto = true;
-      }
-      if (ns.corporation.hasResearched(div.name, "MarketTA2")) {
-        ns.corporation.setProductMarketTA2(div.name, name, true);
-        auto = true;
-      }
-      if (auto) {
-        continue;
+      // If we have a good AI, we're all good
+      if (ns.corporation.hasResearched(div.name, "Market-TA.II")) {
+        ns.corporation.setProductMarketTA1(div.name, name, false);
+        ns.corporation.setProductMarketTA2(div.name, name, false);
+        // continue;
       }
       if (p.sCost == 0) {
         // Set a default
-        ns.corporation.sellProduct(div.name, "Aevum", name, "MAX", "MP*10", true);
+        ns.corporation.sellProduct(div.name, researchCity, name, "MAX", "MP*10", true);
         continue;
       }
       let stk = Object.values(p.cityData);
       let qty = Math.max(...stk.map(s => s[0].toFixed(2)));
-      let deltas = stk.map(s => (s[1]-s[2]).toFixed(2));
+      let deltas = stk.map(s => (s[1] - s[2]).toFixed(2));
       let min = Math.min(...deltas);
       let max = Math.max(...deltas);
       let m = Number(p.sCost.substr(3));
-      ns.print(`Previous multiplier = ${m}`);
+      let changed = false;
       if (min > 10 || qty > 1000) {  // price is too high
         m *= .8;
+        await netLog(ns, "Reducing price of %s to MP*%.2f", name, m);
+        changed = true;
       } else if (max < -10 || qty < 10) {
-        m *= 2;
+        m *= 1.5;
+        await netLog(ns, "Increasing price of %s to MP*%.2f", name, m);
+        changed = true;
       }
-      m ||= 10;
-      ns.print(`Setting price of ${name} to MP*${m}`);
-      ns.corporation.sellProduct(div.name, "Aevum", name, "MAX", "MP*"+ m, true);
+      if (m == 0) {
+        m = 10;
+        changed = true;
+      }
+      if (changed) {
+        await netLog(ns, `Setting price of ${name} to MP*${m.toFixed(2)}`);
+        ns.corporation.sellProduct(div.name, researchCity, name, "MAX", "MP*" + m.toFixed(2), true);
+      }
     }
   }
 }
@@ -117,8 +253,18 @@ async function devProducts(ns) {
   const products = {
     "Tobacco": {
       name: "Totally Safe Inc",
-      products: [ "Total Lungs", "Total Heart", "Total Voice" ],
-      gens: [ "", "II", "Delux", "Classic", "Ltd Ed", "2000", "Retro", "X-3000", "DLX" ],
+      products: ["Total Lungs", "Total Heart", "Total Voice"],
+      gens: ["", "II", "Delux", "Classic", "Ltd Ed", "2000", "Retro", "X-3000", "DLX"],
+    },
+    "Software": {
+      name: "Totally Works Inc",
+      products: ["Total Vaporware", "Total Feature", "Total VR"],
+      gens: ["0.1", "0.5", "beta", "1.0", "Enterprise", "Delux", "2000", "25"],
+    },
+    "Food": {
+      name: "Totally Organic Inc",
+      products: ["Total Food Product", "Total Drink Product", "Total Nutrition"],
+      gens: ["", "II", "Delux", "Organic", "Zero", "Free", "Diet"],
     },
   }
   let progress = {};
@@ -130,14 +276,14 @@ async function devProducts(ns) {
     }
     let prods = div.products.map(p => ns.corporation.getProduct(div.name, p));
     let p = Math.min(...prods.map(p => p.developmentProgress));
-    if (p < 100 && div.products.length == 3) {
-      progress[div.name] = p/100;
+    if (p < 100) {
+      progress[div.name] = p / 100;
       continue;
     }
 
     let budget = corp.funds / 100;
     if (budget < 1e9) {
-      netLog(ns, "Not enough funds to create new products for %s", div.name);
+      await netLog(ns, "Not enough funds to create new products for %s", div.name);
       return progress;
     }
 
@@ -145,19 +291,22 @@ async function devProducts(ns) {
     if (!pName) {
       return;
     }
-    if (div.products.length == 3) {
-      let dName = div.products.map(p => [p, ns.corporation.getProduct(div.name, p).sCost])
-        .sort((a,b) => a[1] < b[1] ? -1 : 1)
+    let notice = ns.corporation.getCorporation().revenue < 1e9 ? toast : netLog;
+    let productLimit = 3;
+    if (ns.corporation.hasResearched(div.name, "uPgrade: Capacity.I")) { productLimit++; }
+    if (ns.corporation.hasResearched(div.name, "uPgrade: Capacity.II")) { productLimit++; }
+    if (div.products.length == productLimit) {
+      let dName = div.products.map(p => [p, ns.corporation.getProduct(div.name, p).dmd])
+        .sort((a, b) => a[1] - b[1])
         .map(p => p[0])[0];
-      await toast(ns, "Discontinuing product for %s: %s", div.name, dName);
+      await notice(ns, "Discontinuing product for %s: %s", div.name, dName);
       ns.corporation.discontinueProduct(div.name, dName);
     }
 
-    await toast(ns, "Creating new product for %s: %s", div.name, pName);
-    ns.corporation.makeProduct(div.name, "Aevum", pName, budget, budget);
+    await notice(ns, "Creating new product for %s: %s", div.name, pName);
+    ns.corporation.makeProduct(div.name, researchCity, pName, budget, budget);
     progress[div.name] = 0;
   }
-
   return progress;
 }
 
@@ -169,14 +318,14 @@ function getNextProduct(def, cur) {
   for (let c of cur) {
     let type = def.products.find(p => c.startsWith(p))
     let iter = type.split("/")[1] || 0;
-    let gen = c.substr(type.length+1).split("/")[0] || "";
+    let gen = c.substr(type.length + 1).split("/")[0] || "";
     let tID = def.products.indexOf(type);
     let gID = def.gens.indexOf(gen);
-    type = def.products[tID+1];
-    if (!type) { 
+    type = def.products[tID + 1];
+    if (!type) {
       // next gen!
       type = def.products[0];
-      gen = def.gens[gID+1];
+      gen = def.gens[gID + 1];
       if (!gen) {
         iter++;
         gen = def.gens[0] + "/" + iter;
@@ -189,4 +338,45 @@ function getNextProduct(def, cur) {
   }
 
   return false;
+}
+
+/**
+ * @param {NS} ns
+ * @param {string} name
+ */
+async function hireResearchers(ns, name) {
+  // get office size
+  const size = ns.corporation.getOffice(name, researchCity).size;
+  let sets = Math.floor(size / 5);
+  let left = size % 5;
+  let target = {
+    "Business": sets,
+    "Engineer": sets + left,
+    "Operations": sets,
+    "Management": sets,
+    "Research & Development": sets,
+  }
+
+  await expandOffice(ns, name, researchCity, target);
+}
+
+/**
+ * @param {NS} ns
+ * @param {string} name
+ * @param {string} city
+ */
+async function hireSales(ns, name, city) {
+  // get office size
+  const size = ns.corporation.getOffice(name, city).size;
+  let sets = Math.floor(size / 9);
+  let left = size % 9;
+  let target = {
+    "Business": sets,
+    "Engineer": sets * 2 + left,
+    "Operations": sets * 2,
+    "Management": sets * 2,
+    "Research & Development": sets * 2,
+  }
+
+  await expandOffice(ns, name, city, target);
 }
