b03bc61b0823294088a1dc021014b6febf4386aa
Dan Boger <dan@peeron.com>
2025-12-27 13:23:11 -0800

    Remove the trivial RPC servers that are now auto-generated

--- 2025/lib/rpc/getOfficeSizeUpgradeCost.js
+++
@@ -1,9 +0,0 @@
-import { dn } from "@/lib/dn.js"
-
-/** @param {NS} ons */
-export async function main(ons) {
-  /** @type {NS} ns */
-  let ns = new dn(ons)
-
-  await ns.listen("corporation_getOfficeSizeUpgradeCost", ons.corporation.getOfficeSizeUpgradeCost)
-}
