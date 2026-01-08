82c3ea4e1995800d18db84ef8ccdad511cbd4d0e
Dan Boger <dan@peeron.com>
2026-01-07 22:42:16 -0800

    Add a sidebar to track incoming funds

--- 2025/status.js
+++ 2025/status.js
@@ -2,18 +2,70 @@ import { dns } from "@/hosts.js"
 import { table } from "@/table.js"
 /** @param {NS} ns */
 export async function main(ns) {
+  [
+    "getServerBaseSecurityLevel",
+    "getServerMinSecurityLevel",
+    "getServerMoneyAvailable",
+    "getServerSecurityLevel",
+    "getServerMaxRam",
+    "getServerUsedRam",
+  ].forEach(f => ns.disableLog(f))
   var hosts = dns(ns)
-  var ps = []
   var allMem = 0
   var usedMem = 0
   for (var h of hosts.keys()) {
-    ps.push(...ns.ps(h))
     allMem += ns.getServerMaxRam(h)
     usedMem += ns.getServerUsedRam(h)
   }
 
-  var summary = new Map()
+  var summary = collectData(ns)
+
+  let player = ns.getPlayer()
+  const sum = (l) => l.reduce((a,i) => a + i, 0)
+  const details = (procs) => [
+    sum(procs),
+    procs.length > 0 ? Math.min(...procs) : 0,
+    procs.length > 0 ? Math.max(...procs) : 0,
+    procs.length
+  ]
+  const loot = (ent) => 
+    ns.fileExists("Formulas.exe") && ent.h.length > 0 ?
+      "$" + ns.formatNumber(
+        ns.formulas.hacking.hackPercent(ent.srv, player) * sum(ent.h) * ent.mon
+      ) : ent.hack * sum(ent.h)
+
+  ns.tprint(
+    table(
+      ns,
+      ["Target", "Hack", "Grow", "Weaken", "Take", "Money", "Security"],
+      Array.from(summary.keys()).sort().map((h) => [
+        summary.get(h).host,
+        [ns.sprintf("%d (%d-%d, %d hosts)", ...details(summary.get(h).h))],
+        [ns.sprintf("%d (%d-%d, %d hosts)", ...details(summary.get(h).g))],
+        [ns.sprintf("%d (%d-%d, %d hosts)", ...details(summary.get(h).w))],
+        ns.sprintf("%s (%s)", loot(summary.get(h)),
+          summary.get(h).h.length > 0 ? ns.tFormat(summary.get(h).when || 0) : "-"),
+        "$" + ns.formatNumber(summary.get(h).mon),
+        summary.get(h).sec,
+      ])
+    )
+  )
+  ns.tprintf(
+    "Util: %s/%s (%%%d)",
+    ns.formatRam(usedMem),
+    ns.formatRam(allMem),
+    100*usedMem/allMem,
+  )
+}
+
+export function collectData(ns) {
+  var hosts = dns(ns)
+  var ps = []
+  for (var h of hosts.keys()) {
+    ps.push(...ns.ps(h))
+  }
   var now = Date.now()
+  var summary = new Map()
   for (var p of ps) {
     if (!["bin/grow.js", "bin/hack.js", "bin/weaken.js"].includes(p.filename)) {
       continue
@@ -40,40 +92,5 @@ export async function main(ns) {
     summary.set(target, ent)
   }
 
-  let player = ns.getPlayer()
-  const sum = (l) => l.reduce((a,i) => a + i, 0)
-  const details = (procs) => [
-    sum(procs),
-    procs.length > 0 ? Math.min(...procs) : 0,
-    procs.length > 0 ? Math.max(...procs) : 0,
-    procs.length
-  ]
-  const loot = (ent) => 
-    ns.fileExists("Formulas.exe") && ent.h.length > 0 ?
-      "$" + ns.formatNumber(
-        ns.formulas.hacking.hackPercent(ent.srv, player) * sum(ent.h) * ent.mon
-      ) : ent.hack * sum(ent.h)
-
-  ns.tprint(
-    table(
-      ns,
-      ["Target", "Hack", "Grow", "Weaken", "Take", "Money", "Security"],
-      Array.from(summary.keys()).sort().map((h) => [
-        summary.get(h).host,
-        [ns.sprintf("%d (%d-%d, %d hosts)", ...details(summary.get(h).h))],
-        [ns.sprintf("%d (%d-%d, %d hosts)", ...details(summary.get(h).g))],
-        [ns.sprintf("%d (%d-%d, %d hosts)", ...details(summary.get(h).w))],
-        ns.sprintf("%s (%s)", loot(summary.get(h)),
-          summary.get(h).h.length > 0 ? ns.tFormat(summary.get(h).when) : "-"),
-        "$" + ns.formatNumber(summary.get(h).mon),
-        summary.get(h).sec,
-      ])
-    )
-  )
-  ns.tprintf(
-    "Util: %s/%s (%%%d)",
-    ns.formatRam(usedMem),
-    ns.formatRam(allMem),
-    100*usedMem/allMem,
-  )
+  return summary
 }
