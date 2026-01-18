4f28eb6bf30ead07d366ba1cda8d9f9b28607263
Dan Boger <dan@peeron.com>
2026-01-17 19:08:58 -0800

    Commit viteburner configs that actually manage to push the subdirectory of the git repo, because figuring that out took a while.

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+/* eslint-env node */
+import { defineConfig } from 'viteburner';
+import { resolve } from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'bitburner/2025'),
+      '/src': resolve(__dirname, '/'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: false,
+  },
+  viteburner: {
+    watch: [{
+      pattern: 'bitburner/2025/**/*.{js,ts,jsx,tsx}',
+      transform: true,
+      location: (file) => [{
+        filename: file.replace(/^bitburner\/2025/, ''),
+        server: "home"
+      }]
+    }],
+    sourcemap: 'inline',
+  },
+});
